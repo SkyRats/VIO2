254b286aa635cfb212632159495dfc5274c3482c
Fuoco <pedrofuoco6@gmail.com>
2022-08-15 20:32:47 -0300

    topic name fixed

--- scripts/bridge.py
+++ scripts/bridge.py
@@ -10,7 +10,7 @@ class realsense_bridge(Node):
     def __init__(self):
         super().__init__('realsense_bridge')
         self.odometry = Odometry()
-        self.odometry_sub = self.create_subscription(Odometry, '/camera/pose/sample', self.odometry_callback, qos.qos_profile_sensor_data)
+        self.odometry_sub = self.create_subscription(Odometry, '/camera/odom/sample', self.odometry_callback, qos.qos_profile_sensor_data)
         self.odometry_pub = self.create_publisher(Odometry,  '/mavros/odometry/out', 5)
     
     def odometry_callback(self, data):
